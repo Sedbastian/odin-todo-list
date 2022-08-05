212e03875100bbc51d8a5c3a2db13e5f8696b405
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-08-04 21:34:52 -0300

    Agregar crearTarea

--- src/dommodule.js
+++ src/dommodule.js
@@ -1,4 +1,4 @@
-import {crearProyecto} from "./index.js";
+import {crearProyecto, crearTarea} from "./index.js";
 
 function domCrearProyecto() {
     function llamarCrearProyecto() {
@@ -13,10 +13,11 @@ function domCrearProyecto() {
     }
 
     const divFormulario = document.createElement("div");
-    divFormulario.classList.add("divFormulario");
+    divFormulario.classList.add("divFormularioProyecto");
 
     const crearNuevoProyecto = document.createElement("h2");
     crearNuevoProyecto.textContent = "Crear Nuevo Proyecto";
+    divFormulario.appendChild(crearNuevaTarea);
 
     const labelTitulo =  document.createElement("label");
     labelTitulo.setAttribute("for", "titulo");
@@ -55,10 +56,69 @@ function domCrearProyecto() {
 };
 
 function domCrearTarea() {
+    function llamarCrearTarea() {
+        const titulo = `${inputTitulo.value}`;
+        const descripcion = `${inputDescripcion.value}`;
+        const proyecto = `${inputProyecto.value}`;
+        crearTarea(titulo, descripcion, proyecto);
+        cuerpi.removeChild(divFormulario);
+    };
+
+    function cancelarFormulario() {
+        cuerpi.removeChild(divFormulario);
+    }
+
+    const divFormulario = document.createElement("div");
+    divFormulario.classList.add("divFormularioTarea");
+
+    const crearNuevaTarea = document.createElement("h2");
+    crearNuevaTarea.textContent = "Crear Nueva Tarea";
+    divFormulario.appendChild(crearNuevaTarea);
+
+    const labelTitulo =  document.createElement("label");
+    labelTitulo.setAttribute("for", "titulo");
+    labelTitulo.textContent = "Titulo:";
+    divFormulario.appendChild(labelTitulo);
+
+    const inputTitulo = document.createElement("input");
+    inputTitulo.setAttribute("type", "text");
+    inputTitulo.setAttribute("id", "titulo");
+    divFormulario.appendChild(inputTitulo);
+
+    const labelDescripcion =  document.createElement("label");
+    labelDescripcion.setAttribute("for", "descripcion");
+    labelDescripcion.textContent = "Descripcion:";
+    divFormulario.appendChild(labelDescripcion);
+
+    const inputDescripcion = document.createElement("input");
+    inputDescripcion.setAttribute("type", "text");
+    inputDescripcion.setAttribute("id", "descripcion");
+    divFormulario.appendChild(inputDescripcion);
+
+    const labelProyecto =  document.createElement("label");
+    labelProyecto.setAttribute("for", "proyecto");
+    labelProyecto.textContent = "Proyecto al que pertenece:";
+    divFormulario.appendChild(labelProyecto);
+
+    const inputProyecto = document.createElement("input");
+    inputProyecto.setAttribute("type", "text");
+    inputProyecto.setAttribute("id", "proyecto");
+    divFormulario.appendChild(inputProyecto);
+
+    const botonCrear = document.createElement("button");
+    botonCrear.addEventListener("click", llamarCrearTarea);
+    botonCrear.setAttribute("class", "botonFormulario");
+    botonCrear.textContent = "Crear Tarea";
+    divFormulario.appendChild(botonCrear);
+
+    const botonCancelar = document.createElement("button");
+    botonCancelar.addEventListener("click", cancelarFormulario);
+    botonCancelar.setAttribute("class", "botonFormulario");
+    botonCancelar.textContent = "Cancelar";
+    divFormulario.appendChild(botonCancelar);
+
     const cuerpi = document.querySelector("body");
-    const tareaEnDom = document.createElement("div");
-    tareaEnDom.textContent = prompt("Titulo de la Tarea");
-    cuerpi.appendChild(tareaEnDom);
+    cuerpi.appendChild(divFormulario);
 };
 
 export {
--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const proyectos = {};
 nuevoProyecto(fabricarProyecto("Tareas Sueltas", "Estas son Tareas que no están relacionadas con ningún proyecto en especial."));
 
 proyectos["Tareas Sueltas"].fabricarTarea("Primera Tarea", "Solo una prueba", "7/7", "mediana");
-proyectos["Tareas Sueltas"].fabricarTarea("Segunda Tarea", "Segunda prueba", 8/8, "baja");
+proyectos["Tareas Sueltas"].fabricarTarea("Segunda Tarea", "Segunda prueba", "8/8", "baja");
 
 console.log(proyectos);
 
@@ -30,7 +30,13 @@ function crearProyecto(titulo, descripcion) {
     console.log(proyectos);
 };
 
+// A esta funcion la termina llamando el eventListener del boton crearTarea.
+function crearTarea(titulo, descripcion, proyecto) {
+    proyectos[proyecto].fabricarTarea(titulo, descripcion);
+    console.log(proyectos);
+};
+
 document.querySelector(".proyectoNuevo").addEventListener("click", domCrearProyecto);
 document.querySelector(".tareaNueva").addEventListener("click", domCrearTarea);
 
-export {crearProyecto};
+export {crearProyecto, crearTarea};
